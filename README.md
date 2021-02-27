fb4fee7c3a4ce611998056f3a66ca79cc124ccb2
Mike Wohlgemuth <woogie@woogie.net>
2021-02-26 21:19:19 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,192 @@
+# unifi-events 
+
+[![License][mit-badge]][mit-url]
+
+unifi-axios-events is a Node.js module that allows you to listen for events from and call methods on the UniFi API (UniFi is Ubiquiti Networks wifi controller software). It is heavily derivitive of [unifi-events](https://github.com/oznu/unifi-events). I was having issues with it interfacing with my UDM Pro and ended up porting the code to Axios since request is now deprecated. I should work with any Unifi controller but I have only tested it with the UDM Pro.
+
+## Requirements
+
+* Node.js v6 or later
+* [UniFi-Controller](https://www.ubnt.com/download/unifi) v5
+
+## Installation
+
+`$ npm install https://github.com/worldwidewoogie/unifi-axios-events.git`
+
+## Example
+
+```javascript
+const Unifi = require('unifi-axios-events')
+
+const unifi = new Unifi({
+  host: 'unifi',                        // The hostname or ip address of the unifi controller (default: 'unifi')
+  port: 8443,                           // Port of the unifi controller (default: 8443)
+  username: 'admin',                    // Username (default: 'admin').
+  password: 'ubnt',                     // Password (default: 'ubnt').
+  site: 'default',                      // The UniFi site to connect to (default: 'default').
+  insecure: true,                       // Allow connections if SSL certificate check fails (default: false).
+});
+
+unifi.init();
+
+// Listen for any event
+unifi.on('**', function (data) {
+  console.log(this.event, data);
+});
+```
+
+## Events
+
+unifi-events uses [EventEmitter2](https://github.com/asyncly/EventEmitter2) and namespaced events. 
+
+### namespace `ctrl`
+
+These events indicate the status of the connection to the UniFi controller
+
+* `ctrl.connect` - emitted when the connection to the controller is established
+* `ctrl.disconnect` - emitted when the connection to the controller is lost
+* `ctrl.error` - 
+* `ctrl.reconnect` - 
+
+
+### namespaces `wu`, `wg`, `lu`, ...
+
+This JSON file shows all possible events: https://demo.ubnt.com/manage/locales/en/eventStrings.json?v=5.4.11.2
+The prefix `EVT_` gets stripped, the first underscore is replaced by the namespace separating dot, everything is 
+converted to lower case. Some events such as `EVT_AD_LOGIN` (Admin Login) are not emitted by the UniFi Controller.
+
+
+#### Example Wireless User events
+
+* `wu.connected` - Wireless User connected
+* `wu.disconnected` - Wireless User disconnected
+* `wu.roam` - Wireless User roamed from one AP to another
+* `wu.roam_radio` - Wireless User changed channel on the same AP
+
+#### Example Wireless Guest Events
+
+* `wg.connected` - Wireless Guest connected
+* `wg.disconnected` - Wireless Guest disconnected
+* `wg.roam` - Wireless Guest roamed from one AP to another
+* `wg.roam_radio` - Wireless Guest changed channel on the same AP
+* `wg.authorization_ended` - Wireless Guest became unauthorised
+
+#### Wildcard usage
+
+Example listing for events on Guest Wireless networks only:
+
+```javascript
+unifi.on('wg.*', function (data) {
+  console.log(this.event, data);
+})
+```
+
+Example listening for connected events on all network types:
+
+```javascript
+unifi.on('*.connected', function (data) {
+  console.log(this.event, data);
+});
+```
+
+## Methods
+
+#### connect()
+
+Connect to the UniFi controller. Is called in the constructor, so normally you don't need to call it (except if you want 
+to re-establish a connection that was closed before).
+
+#### close()
+
+Closes the connection to the UniFi controller
+
+
+### UniFi API Methods
+
+Following methods operate on the configured site. The path gets prefixed with 
+`https://<host>:<port>/api/s/<site>/` if it does not start with a slash, otherwise it gets prefixed with 
+`https://<host>:<port>`. To explore available API endpoints you can use the 
+[UniFi-API-browser](https://github.com/Art-of-WiFi/UniFi-API-browser).
+
+These methods are returning a promise.
+
+
+#### get(path)
+
+Do a HTTP GET on the API.
+
+**Examples:**
+
+* Get a list of all clients
+```javascript
+unifi.get('stat/sta').then(console.log);
+```
+
+* Get infos of a specific client
+```javascript
+unifi.get('stat/user/<mac>').then(console.log);
+```
+
+* Get alarms
+```javascript
+unifi.get('list/alarm').then(console.log);
+```
+
+* Get wireless network IDs
+```javascript
+unifi.get('rest/wlanconf').then(res => {
+    res.data.forEach(wlan => {
+        console.log(wlan.name, wlan._id);
+    });
+});
+```
+
+* Get device IDs
+```javascript
+unifi.get('stat/device').then(res => {
+     res.data.forEach(dev => {
+        console.log(dev.name, dev._id);
+     });
+});
+```
+
+#### del(path)
+
+Do a HTTP DELETE on the API.
+
+#### post(path, body)
+
+Do a HTTP POST on the API.
+
+**Examples:**
+
+* Enable all LEDs of all APs
+```javascript
+unifi.post('set/setting/mgmt', {led_enabled: true}).then(console.log);
+```
+
+* Disable a WLAN
+```javascript
+unifi.post('upd/wlanconf/<wlan_id>', {enabled: false}).then(console.log);
+```
+
+#### put(path, body)
+
+Do a HTTP PUT on the API.
+
+**Examples:**
+
+* Enable LED of AP
+```javascript
+unifi.put('rest/device/<device_id>', {led_override: 'on'}).then(console.log);
+```
+
+
+## License
+
+* MIT © 2021 woogie (https://github.com/worldwidewoogie)
+* MIT © 2017-2021 [oznu](https://github.com/oznu)
+* MIT © 2018 [Sebastian Raff](https://github.com/hobbyquaker)    
+
+[mit-badge]: https://img.shields.io/badge/License-MIT-blue.svg?style=flat
+[mit-url]: LICENSE
---
+++ index.js
@@ -0,0 +1,274 @@
+"use strict"
+
+const url = require('url')
+const EventEmitter = require('eventemitter2').EventEmitter2
+const https = require('https')
+const WebSocket = require('ws')
+const axios = require('axios')
+const axiosCookieJarSupport = require('axios-cookiejar-support').default
+const tough = require('tough-cookie')
+axiosCookieJarSupport(axios)
+
+module.exports = class unifiEvents extends EventEmitter {
+
+    constructor(opts) {
+        super({
+            wildcard: true
+        })
+
+        this.opts = opts || {}
+        this.opts.host = this.opts.host || 'unifi'
+        this.opts.port = this.opts.port || 8443
+        this.opts.username = this.opts.username || 'admin'
+        this.opts.password = this.opts.password || 'ubnt'
+        this.opts.site = this.opts.site || 'default'
+        this.opts.insecure = this.opts.insecure || false
+        this.opts.listen = this.opts.listen || true
+
+        this.controller = url.parse(`https://${opts.host}:${opts.port}`)
+
+        this.cookieJar = new tough.CookieJar()
+        this.isClosed = true
+        this.autoReconnectInterval = 5 * 1000
+
+        this.isInit = false
+    }
+
+    init() {
+        return new Promise((resolve, reject) => {
+            if (this.isInit) {
+                resolve(true)
+            } else {
+                this.instance = axios.create({
+                    jar: this.cookieJar,
+                    withCredentials: true,
+                    httpsAgent: new https.Agent({ rejectUnauthorized: false, requestCert: true, keepAlive: true })
+                })
+                this.instance.get(this.controller).then(res => {
+                    if (res.headers['x-csrf-token']) {
+                        this.xcsrftoken = res.headers['x-csrf-token']
+                        this.instance.defaults.headers.delete['x-csrf-token'] = this.xcsrftoken
+                        this.instance.defaults.headers.post['x-csrf-token'] = this.xcsrftoken
+                        this.instance.defaults.headers.put['x-csrf-token'] = this.xcsrftoken
+                        this.unifios = true
+                    } else {
+                        this.unifios = false
+                    }
+                    this.isInit = true
+                    this.connect()
+                    resolve(true)
+                })
+            }
+        })
+    }
+
+    connect(reconnect) {
+        return new Promise((resolve, reject) => {
+            this.isClosed = false
+            this._login(reconnect).then(() => {
+                this._listen()
+                resolve(true)
+            })
+        })
+    }
+
+    close() {
+        this.isClosed = true
+        this.ws.site.close()
+        this.ws.super.close()
+        this.ws.system.close()
+    }
+
+    _login(reconnect) {
+        return new Promise((resolve, reject) => {
+            let endpointUrl = `${this.controller.href}api/login`
+            if (this.unifios) {
+                endpointUrl = `${this.controller.href}api/auth/login`
+            }
+            this.instance.post(endpointUrl, {
+                username: this.opts.username,
+                password: this.opts.password,
+            }).then(() => {
+                resolve(true)
+            }).catch(error => {
+                let message = ''
+                if (error.response) {
+                    message = error.response.status + ' ' + error.response.statusText
+                } else {
+                    message = error
+                }
+                if (!reconnect) {
+                    this._reconnect()
+                }
+            })
+        })
+    }
+
+    _listen() {
+        if (this.opts.listen) {
+
+            this.cookieJar.getCookieString(this.controller.href).then(cookies => {
+
+                let eventsUrl = `wss://${this.controller.host}/wss/s/${this.opts.site}/events`
+
+                if (this.unifios) {
+                    eventsUrl = `wss://${this.controller.host}/proxy/network/wss/s/${this.opts.site}/events`
+                }
+
+                this.ws = new WebSocket(eventsUrl, {
+                    perMessageDeflate: false,
+                    rejectUnauthorized: !this.opts.insecure,
+                    headers: {
+                        Cookie: cookies
+                    }
+                })
+
+                const pingpong = setInterval(() => {
+                    //console.dir("sending ping")
+                    this.ws.send('ping')
+                }, 15000)
+
+                this.ws.on('open', () => {
+                    //console.dir(this.ws, { depth: null })
+                    this.isReconnecting = false
+                    this.emit('ctrl.connect')
+                })
+
+                this.ws.on('message', data => {
+                    if (data === 'pong') {
+                        return
+                    }
+                    try {
+                        const parsed = JSON.parse(data)
+                        if ('data' in parsed && Array.isArray(parsed.data)) {
+                            parsed.data.forEach(entry => {
+                                this._event(entry)
+                            })
+                        }
+                    } catch (err) {
+                        this.emit('ctrl.error', err)
+                    }
+                })
+
+                this.ws.on('close', () => {
+                    this.emit('ctrl.close')
+                    clearInterval(pingpong)
+                    this._reconnect()
+                })
+
+                this.ws.on('error', err => {
+                    this.emit('ctrl.error', err)
+                    clearInterval(pingpong)
+                    this._reconnect()
+                })
+            })
+        }
+    }
+
+    _reconnect() {
+        if (!this.isReconnecting && !this.isClosed) {
+            this.isReconnecting = true
+            setTimeout(() => {
+                this.emit('ctrl.reconnect')
+                this.isReconnecting = false
+                this.connect(true)
+            }, this.autoReconnectInterval)
+        }
+    }
+
+    _event(data) {
+        if (data && data.key) {
+            // TODO clarifiy what to do with events without key...
+            const match = data.key.match(/EVT_([A-Z]{2})_(.*)/)
+            if (match) {
+                const [, group, event] = match
+                this.emit([group.toLowerCase(), event.toLowerCase()].join('.'), data)
+            }
+        }
+    }
+
+    _ensureLoggedIn() {
+        return new Promise((resolve, reject) => {
+            this.instance.get(`${this.controller.href}api/${this.unifios ? 'users/' : ''}self`).then(() => {
+                resolve(true)
+            }).catch(() => {
+                this._login().then(() => {
+                    resolve(true)
+                }).catch(error => {
+                    reject(error)
+                })
+            })
+        })
+    }
+
+    _url(path) {
+        if (this.unifios) {
+            if (path.indexOf('/') === 0) {
+                return `${this.controller.href}proxy/network/${path}`
+            }
+            return `${this.controller.href}proxy/network/api/s/${this.opts.site}/${path}`
+        }
+        else {
+            if (path.indexOf('/') === 0) {
+                return `${this.controller.href}${path}`
+            }
+            return `${this.controller.href}api/s/${this.opts.site}/${path}`
+        }
+    }
+
+    get(path) {
+        return new Promise((resolve, reject) => {
+            this._ensureLoggedIn().then(() => {
+                this.instance.get(this._url(path)).then(response => {
+                    resolve(response.data)
+                }).catch(error => {
+                    reject(error)
+                })
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    }
+
+    del(path) {
+        return new Promise((resolve, reject) => {
+            this._ensureLoggedIn().then(() => {
+                this.instance.del(this._url(path)).then(response => {
+                    resolve(response.data)
+                }).catch(error => {
+                    reject(error)
+                })
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    }
+
+    post(path, body) {
+        return new Promise((resolve, reject) => {
+            this._ensureLoggedIn().then(() => {
+                this.instance.post(this._url(path), { body }).then(response => {
+                    resolve(response.data)
+                }).catch(error => {
+                    reject(error)
+                })
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    }
+
+    put(path, body) {
+        return new Promise((resolve, reject) => {
+            this._ensureLoggedIn().then(() => {
+                this.instance.put(this._url(path), { body }).then(response => {
+                    resolve(response.data)
+                }).catch(error => {
+                    reject(error)
+                })
+            }).catch(error => {
+                reject(error)
+            })
+        })
+    }
+}
